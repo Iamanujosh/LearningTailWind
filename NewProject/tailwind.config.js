6bc78caac63690b066b9ef1d2654058b4631c57b
anu <joshi2017anu@gmail.com>
2025-01-19 17:33:00 +0530

    completed

--- NewProject/tailwind.config.js
+++ NewProject/tailwind.config.js
@@ -2,8 +2,30 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        fadeOut: {
+          '0%': { opacity: '1' }, // Fully visible
+          '100%': { opacity: '0' } // Fully invisible
+        },
+        wiggle: {
+          '0%': { transform: 'translate(-20px,0px)' },
+          '50%': { transform: 'translate(50px,0px)'},
+          '100%': { transform: 'translate(-20px,0px)'},
+          
+        },
+        ziggle: {
+          '0%': { transform: 'translate(20px,0px)' },
+          '50%': { transform: 'translate(-50px,0px)'},
+          '100%': { transform: 'translate(20px,0px)'},
+        }
+      },
+      animation: {
+        fadeOut: 'fadeOut 5s forwards',
+        wiggle: 'wiggle 10s ease-in-out infinite',
+        ziggle: 'ziggle 10s ease-in-out infinite', // Adjust duration as needed
+      },
+    },
   },
   plugins: [],
 }
-
